24f637cb3a2bea54c6118851c2ca33d2aba32903
Arun Kumar <java.kumar.arun@gmail.com>
2020-01-08 11:21:05 +0530

    num is updated

--- src/com/demo/TableDemo.java
+++ src/com/demo/TableDemo.java
@@ -3,7 +3,7 @@ package com.demo;
 public class TableDemo {
 
 	public static void main(String[] args) {
-		int num = 5;
+		int num = 23;
 		for(int i=1;i<=10;i++) {
 			System.out.println(num+"*"+i+"="+(num*i));
 		}
